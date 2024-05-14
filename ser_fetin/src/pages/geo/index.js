267112981e5b4b77d31cae826b338fb188cc5ec9
BrunaDev <dev.bruna.magalhaes@gmail.com>
2024-05-14 10:51:49 -0300

    :construction: adding geolocation

--- ser_fetin/src/pages/geo/index.js
+++ ser_fetin/src/pages/geo/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState, useRef } from 'react';
 import {
     KeyboardAvoidingView,
     Text,
@@ -5,21 +6,75 @@ import {
     TouchableOpacity,
     Platform
 } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-import { styles } from '../../styles/home/styles';
+import MapView, { Marker } from 'react-native-maps';
 
-import { useNavigation } from '@react-navigation/native';
-import * as Animatable from 'react-native-animatable';
+import { 
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    LocationObject,
+    watchPositionAsync,
+    LocationAccuracy
+} from 'expo-location';
+
+import { styles } from '../../styles/geo/styles';
 
 export default function Geolocalization(){
+    const [location, setLocation] = useState<LocationObject | null>(null);
+    const mapRef = useRef<MapView>(null);
 
-    const navigation = useNavigation();
+    async function requestLocationPermissions(){
+        //verifica se usuario deu permissão
+        const { granted } = await requestForegroundPermissionsAsync();
+
+        if(granted){
+            //posição atual
+            const currentPosition = await getCurrentPositionAsync();
+            setLocation(currentPosition);
+        }
+    }
+
+    useEffect(() => {
+        requestLocationPermissions();
+    }, []);
+
+    useEffect(() => {
+        watchPositionAsync({
+            accuracy: LocationAccuracy.Highest,
+            timeInterval: 1000,
+            distanceInterval: 1
+        }, (response) => {
+            setLocation(response);
+            mapRef.current?.animateCamera({
+                pitch: 20,
+                center: response.coords
+            })
+        });
+    }, []);
 
     return(
         <KeyboardAvoidingView 
             behavior={Platform.OS === "ios" ? "padding" : "height"}
             style={styles.container}
         >
+            {
+                location &&
+                <MapView
+                    ref={mapRef}
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005,
+                    }}
+                >
+                    <Marker coordinate={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                    }} />
+                </MapView>
+            }
+
             <View style={styles.containerHeader}></View>
         </KeyboardAvoidingView>
     )
---
+++ ser_fetin/src/styles/geo/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        flex: 1,
+        widht: '100%',
+    },
+});
